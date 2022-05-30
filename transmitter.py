5213abb216528ec95097adaf4d5ac503eeb948b5
Tom Cummings <tom.cummings07@outlook.com>
2022-05-30 17:46:08 +0100

    Created 'transmitter.py'.
    File created and code added. Code is fully functional. Picks up saved audio files from main.py, converts them to bytes, then a string to be added to JSON, it then adds the file and the necessary data to a JSON file and sends it though a HTTP request.

---
+++ transmitter.py
@@ -0,0 +1,33 @@
+import json, requests, os
+
+# open json system settings file
+systemSettings = json.load(open('settings.json', 'r'))
+
+while True:
+    try:
+        for file in os.listdir('file_data'):
+            name, ext = os.path.splitext(file)
+            if ext == ".json":
+                dataFile = json.load(open(f'file_data/{file}', 'r'))
+
+                audioFileBytes = open(f'recordings/{dataFile["fileName"]}', 'rb').read()
+
+                audioFileString = str(audioFileBytes, 'latin1')
+
+                dataTransmission = {
+                    'startTime': dataFile['startTime'],
+                    'endTime': dataFile['endTime'],
+                    'audioFileString': audioFileString
+                }
+
+                requests.post(url = f'http://{systemSettings["ip_addresses"]["ARWIN_Main_Server"]}', json = dataTransmission)
+
+                os.rename(f'file_data/{file}', f'bin/transmitted_recordings/{file}')
+                os.rename(f'recordings/{dataFile["fileName"]}', f'bin/transmitted_recordings/{dataFile["fileName"]}')
+
+            elif name == ".blank":
+                pass
+            else:
+                os.rename(f'file_data/{file}', f'bin/misplaced_files/{file}')
+    except Exception as e:
+        print(e)
